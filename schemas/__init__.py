3c276d8089fef9432f96d55c495acb93acb0df55
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-09-20 14:30:27 -0400

    fix: fix busca_produto endpoint

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,4 +1,4 @@
 from schemas.produto import ProdutoSchema, ProdutoBuscaSchema, ProdutoViewSchema, \
                             ListagemProdutosSchema, ProdutoDelSchema, apresenta_produtos, \
-                            apresenta_produto, apresenta_produtos
+                            apresenta_produto, apresenta_produtos, mostra_produto
 from schemas.error import ErrorSchema
